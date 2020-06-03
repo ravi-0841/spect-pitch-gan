aa8ab086b77f9ea87beeaa5790177b43e614f7d4
ravi-0841 <ravishankar0841@gmail.com>
2020-06-02 21:23:55 -0400

    analyzing weights for each generator and discriminator

--- submit_multiple_jobs.sh
+++ submit_multiple_jobs.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-cycle_array_pitch=( 0.001 0.01 )
+cycle_array_pitch=( 0.0001 0.001 0.01 )
 cycle_array_mfc=( 0.01 0.1 1 )
-momenta_array=( 1e-06 1e-04 )
+momenta_array=( 1e-06 1e-04 0.001 )
 
 counter=1
 for p in "${cycle_array_pitch[@]}"
@@ -14,7 +14,7 @@ do
             if [ "$counter" -ge 1 ];
             then
                 echo $counter;
-                sbatch -J $counter -o "./txt_files/job${counter}.txt" gen_disc_job_mfc_normalize.sh $p $m $mo
+                sbatch -J $counter -o "./txt_files/job${counter}.txt" gen_disc_job_analyze_weights.sh $p $m $mo
             fi;
             counter=$((counter+1))
         done
