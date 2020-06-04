66c90c7fa88e0bf351c5ec74f850ed48c48ef91b
ravi-0841 <ravishankar0841@gmail.com>
2020-06-04 19:02:43 -0400

    job file and multi-job submission file created for dropout based lr search

---
+++ gen_disc_job_analyze_weights_dropout.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --partition=gpuk80
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=6
+#SBATCH -t 20:00:00
+
+module load cuda/9.0
+module load singularity
+
+# this works on MARCC, work on Lustre /scratch
+cd $HOME/data/ravi/spect-pitch-gan
+
+# pull the image from singularity hub
+singularity pull --name tf.simg shub://ravi-0841/singularity-tensorflow-1.14
+
+# export singularity home path
+export SINGULARITY_HOME=$PWD:/home/$USER
+
+singularity exec --nv ./tf.simg python3 main_analyze_weights_dropout.py --lambda_cycle_pitch $1 --lambda_cycle_mfc $2 --lambda_momenta $3
