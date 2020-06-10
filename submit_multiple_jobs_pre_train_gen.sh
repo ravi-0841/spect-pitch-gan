8438578c1286d3e30b51f016e62499e45807e1bd
ravi-0841 <ravishankar0841@gmail.com>
2020-06-10 15:56:54 -0400

    running pre-trained network with 4 hparams

--- submit_multiple_jobs_pre_train_gen.sh
+++ submit_multiple_jobs_pre_train_gen.sh
@@ -1,22 +1,6 @@
 #! /bin/bash
 
-cycle_array_pitch=( 0.001 0.01 )
-cycle_array_mfc=( 0.01 0.1 1 )
-momenta_array=( 1e-06 1e-04 )
-
-counter=1
-for p in "${cycle_array_pitch[@]}"
-do
-    for m in "${cycle_array_mfc[@]}"
-    do
-        for mo in "${momenta_array[@]}"
-        do
-            if [ "$counter" -ge 1 ];
-            then
-                echo $counter;
-                sbatch -J $counter -o "./txt_files/job${counter}.txt" gen_disc_job_mfc_normalize.sh $p $m $mo
-            fi;
-            counter=$((counter+1))
-        done
-    done
-done
+sbatch -J job_pt1 -o "./txt_files/job_pre_train_1.txt" gen_disc_job_separate_discriminate_pre_train_gen.sh 0.0001 1.0 1e-06 0.0001 0.01
+sbatch -J job_pt2 -o "./txt_files/job_pre_train_2.txt" gen_disc_job_separate_discriminate_pre_train_gen.sh 0.0001 10.0 1e-06 0.0001 0.01
+sbatch -J job_pt3 -o "./txt_files/job_pre_train_3.txt" gen_disc_job_separate_discriminate_pre_train_gen.sh 1e-05 1.0 1e-06 0.0001 0.01
+sbatch -J job_pt4 -o "./txt_files/job_pre_train_4.txt" gen_disc_job_separate_discriminate_pre_train_gen.sh 1e-05 10.0 1e-06 0.0001 0.01
