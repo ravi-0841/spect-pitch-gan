cf2c5eff890dc640a6246ad33ed7bd4e3a9a92fa
ravi-0841 <ravishankar0841@gmail.com>
2020-06-11 16:39:44 -0400

    job file for seq_id created

---
+++ gen_disc_job_sequential_id.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --partition=gpuk80
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=6
+#SBATCH -t 7:00:00
+
+module load cuda/9.0
+module load singularity
+
+# this works on MARCC, work on Lustre /scratch
+cd $HOME/data/ravi/spect-pitch-gan
+
+# pull the image from singularity hub
+singularity pull --name tf.simg shub://ravi-0841/singularity-tensorflow-1.14
+
+# export singularity home path
+export SINGULARITY_HOME=$PWD:/home/$USER
+
+singularity exec --nv ./tf.simg python3 main_sequential.py --lambda_cycle_mfc 10 --lambda_identity_mfc 5
