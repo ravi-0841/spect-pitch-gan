774df0c58ae95c2c7ac4fbacac3881c6b2f2c2e1
ravi-0841 <ravishankar0841@gmail.com>
2020-06-22 16:50:32 -0400

    job file for raw spect main file

---
+++ gen_disc_job_pre_train_raw.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --partition=gpup100
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=6
+#SBATCH -t 12:00:00
+
+module load cuda/9.0
+module load singularity
+
+# this works on MARCC, work on Lustre /scratch
+cd $HOME/data/ravi/spect-pitch-gan
+
+# pull the image from singularity hub
+singularity pull --name tf.simg shub://ravi-0841/singularity-tensorflow-1.14
+
+# export singularity home path
+export SINGULARITY_HOME=$PWD:/home/$USER
+
+singularity exec --nv ./tf.simg python3 main_pre_train_raw.py --lambda_cycle_pitch $1 --lambda_cycle_mfc $2 --lambda_identity_mfc $3 --lambda_momenta $4 --generator_learning_rate $5 --discriminator_learning_rate $6
