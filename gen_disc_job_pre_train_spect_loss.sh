d3db8637738118055fb5f591a0e2c78206238f9f
ravi-0841 <ravishankar0841@gmail.com>
2020-07-03 14:30:29 -0400

    running spect loss on MARCC

--- gen_disc_job_pre_train_spect_loss.sh
+++ gen_disc_job_pre_train_spect_loss.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
-#SBATCH --partition=gpuk80
+#SBATCH --partition=gpup100
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=6
-#SBATCH -t 24:00:00
+#SBATCH -t 12:00:00
 
 module load cuda/10.1
 module load singularity
