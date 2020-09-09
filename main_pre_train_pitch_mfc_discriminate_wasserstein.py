6972154eb59299f433a653a36842bd27c984aa80
ravi-0841 <ravishankar0841@gmail.com>
2020-09-09 16:39:15 -0400

    random seed changed to understand its significance

--- main_pre_train_pitch_mfc_discriminate_wasserstein.py
+++ main_pre_train_pitch_mfc_discriminate_wasserstein.py
@@ -42,7 +42,7 @@ def train(train_dir, model_dir, model_name, random_seed, \
             +"_lmo_"+str(lambda_momenta) \
             +"_lrg_"+str(generator_learning_rate) \
             +"_lrd_"+str(discriminator_learning_rate) + "_li_"\
-            + str(lambda_identity_mfc) + '_pre_trained_pitch_mfc_discriminate_wasserstein'
+            + str(lambda_identity_mfc) + '_pre_trained_pitch_mfc_discriminate_wasserstein_rd_seed_1'
 
     model_dir = os.path.join(model_dir, lc_lm)
 
@@ -242,7 +242,7 @@ if __name__ == '__main__':
 #    validation_dir_default = './data/evaluation/'+emo_pair+"/"+emo_dict[emo_pair][0]
     output_dir_default = './validation_output/'+emo_pair
     tensorboard_log_dir_default = './log/'+emo_pair
-    random_seed_default = 0
+    random_seed_default = 1
 
     parser.add_argument('--train_dir', type=str, help='Directory for A.', 
             default=train_dir_default)
