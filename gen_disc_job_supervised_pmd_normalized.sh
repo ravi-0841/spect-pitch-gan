11fa0096b2f4f5daa3cf87b76f3b8db20ef4b712
ravi-0841 <ravishankar0841@gmail.com>
2020-09-17 13:34:05 -0400

    cmu-arctic nmz mfcc run testing

---
+++ gen_disc_job_supervised_pmd_normalized.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --partition=gpuk80
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=6
+#SBATCH --exclude=gpu001
+#SBATCH -t 48:00:00
+
+module load cuda/10.1
+module load singularity
+
+# this works on MARCC, work on Lustre /scratch
+cd $HOME/data/ravi/spect-pitch-gan
+
+# pull the image from singularity hub
+singularity pull --name tf_1_12.simg shub://ravi-0841/singularity-tensorflow-1.14
+
+# export singularity home path
+export SINGULARITY_HOME=$PWD:/home/$USER
+
+singularity exec --nv ./tf_1_12.simg python3 main_supervised_pmd_normalized.py --lambda_pitch $1 --lambda_mfc $2 --lambda_momenta $3
