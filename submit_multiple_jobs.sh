0b019bcb62fadc18c37571328d7261cae569dedb
ravi-0841 <ravishankar0841@gmail.com>
2020-09-28 14:53:28 -0400

    array non-existent issue in bash script

--- submit_multiple_jobs.sh
+++ submit_multiple_jobs.sh
@@ -6,10 +6,10 @@ cycle_array_energy=( 0.00001 0.001 0.1 )
 counter=1
 for p in "${cycle_array_pitch[@]}"
 do
-    for m in "${cycle_array_mfc[@]}"
+    for e in "${cycle_array_energy[@]}"
     do
         echo $counter;
-        sbatch -J $counter -o "./txt_files/mdw_${counter}.txt" gen_disc_job_energy_f0_momenta_discriminate_wasserstein.sh $p $m
+        sbatch -J $counter -o "./txt_files/mdw_${counter}.txt" gen_disc_job_energy_f0_momenta_discriminate_wasserstein.sh $p $e
         counter=$((counter+1))
     done
 done
