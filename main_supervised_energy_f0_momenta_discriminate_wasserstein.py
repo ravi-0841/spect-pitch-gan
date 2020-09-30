829ab7f790f94d0705d48c4df8903b9adac07194
ravi-0841 <ravishankar0841@gmail.com>
2020-09-29 23:35:38 -0400

    supervised pre-training setup created

---
+++ main_supervised_energy_f0_momenta_discriminate_wasserstein.py
@@ -0,0 +1,214 @@
+import os
+import numpy as np
+import argparse
+import time
+import librosa
+import sys
+import scipy.io.wavfile as scwav
+import scipy.io as scio
+import scipy.signal as scisig
+import pylab
+import logging
+
+from glob import glob
+from nn_models.model_supervised_energy_f0_momenta_discriminate_wasserstein import VariationalCycleGAN
+from utils.helper import smooth, generate_interpolation
+import utils.preprocess as preproc
+from importlib import reload
+from encoder_decoder import AE
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+def train(train_dir, model_dir, model_name, random_seed, \
+            validation_dir, output_dir, \
+            tensorboard_log_dir, pre_train=None, \
+            lambda_energy=0, generator_learning_rate=1e-05, 
+            discriminator_learning_rate=1e-03):
+
+    np.random.seed(random_seed)
+
+    num_epochs = 1000
+    mini_batch_size = 1 # mini_batch_size = 1 is better
+
+    sampling_rate = 16000
+    num_mcep = 23
+    frame_period = 5
+    n_frames = 128
+
+    lc_lm = "le_"+str(lambda_energy)+'_supervised_energy_f0_mwd_mfce'
+
+    model_dir = os.path.join(model_dir, lc_lm)
+
+    logger_file = './log/'+lc_lm+'.log'
+    if os.path.exists(logger_file):
+        os.remove(logger_file)
+
+    reload(logging)
+    logging.basicConfig(filename=logger_file, \
+                            level=logging.DEBUG)
+
+    print("lambda_energy - {}".format(lambda_energy))
+
+    logging.info("lambda_energy - {}".format(lambda_energy))
+    logging.info("generator_lr - {}".format(generator_learning_rate))
+    logging.info("discriminator_lr - {}".format(discriminator_learning_rate))
+
+    if not os.path.isdir("./pitch_spect/"+lc_lm):
+        os.makedirs(os.path.join("./pitch_spect/", lc_lm))
+    else:
+        for f in glob(os.path.join("./pitch_spect/", \
+                lc_lm, "*.png")):
+            os.remove(f)
+    
+    print('Preprocessing Data...')
+
+    start_time = time.time()
+
+    data_train = scio.loadmat(os.path.join(train_dir, 'mfc_energy_cmu_arctic.mat'))
+
+    pitch_A_train = data_train['src_f0_feat']
+    pitch_B_train = data_train['tar_f0_feat']
+    energy_A_train = np.log(data_train['src_ec_feat'])
+    energy_B_train = np.log(data_train['tar_ec_feat'])
+    mfc_A_train = data_train['src_mfc_feat']
+    mfc_B_train = data_train['tar_mfc_feat']
+
+    # Randomly shuffle the trainig data
+    indices_train = np.arange(0, pitch_A_train.shape[0])
+    np.random.shuffle(indices_train)
+    pitch_A_train = pitch_A_train[indices_train]
+    energy_A_train = energy_A_train[indices_train]
+    mfc_A_train = mfc_A_train[indices_train]
+
+    end_time = time.time()
+    time_elapsed = end_time - start_time
+
+    print('Preprocessing Done.')
+
+    print('Time Elapsed for Data Preprocessing: %02d:%02d:%02d' % (time_elapsed // 3600, \
+                                                                   (time_elapsed % 3600 // 60), \
+                                                                   (time_elapsed % 60 // 1)))
+    
+    #use pre_train arg to provide trained model
+    model = VariationalCycleGAN(dim_pitch=1, dim_energy=1, dim_mfc=23, n_frames=n_frames, 
+                                pre_train=pre_train, log_file_name=lc_lm)
+    
+    for epoch in range(1,num_epochs+1):
+
+        print('Epoch: %d' % epoch)
+        logging.info('Epoch: %d' % epoch)
+
+        start_time_epoch = time.time()
+
+        mfc_A, pitch_A, energy_A, \
+            mfc_B, pitch_B, energy_B = preproc.sample_data_energy(mfc_A=mfc_A_train, 
+                    mfc_B=mfc_B_train, pitch_A=pitch_A_train, pitch_B=pitch_B_train, 
+                    energy_A=energy_A_train, energy_B=energy_B_train)
+        
+        n_samples = energy_A.shape[0]
+        
+        train_gen_loss = []
+        train_pitch_loss = []
+        train_energy_loss = []
+
+        for i in range(n_samples // mini_batch_size):
+
+            start = i * mini_batch_size
+            end = (i + 1) * mini_batch_size
+
+            generator_loss, pitch_loss, energy_loss, \
+            gen_pitch_A, gen_energy_A, gen_pitch_B, \
+            gen_energy_B, mom_pitch_A, mom_pitch_B, \
+            mom_energy_A, mom_energy_B \
+                = model.train(mfc_A=mfc_A[start:end], energy_A=energy_A[start:end], 
+                    pitch_A=pitch_A[start:end], mfc_B=mfc_B[start:end],  
+                    energy_B=energy_B[start:end], pitch_B=pitch_B[start:end], 
+                    lambda_energy=lambda_energy, 
+                    generator_learning_rate=generator_learning_rate, 
+                    discriminator_learning_rate=discriminator_learning_rate)
+            
+            train_gen_loss.append(generator_loss)
+            train_pitch_loss.append(pitch_loss)
+            train_energy_loss.append(energy_loss)
+
+        
+        print("Train Generator Loss- {}".format(np.mean(train_gen_loss)))
+        print("Train Pitch Loss- {}".format(np.mean(train_pitch_loss)))
+        print("Train Energy Loss- {}".format(np.mean(train_energy_loss)))
+        
+        logging.info("Train Generator Loss- {}".format(np.mean(train_gen_loss)))
+        logging.info("Train Pitch Loss- {}".format(np.mean(train_pitch_loss)))
+        logging.info("Train Energy Loss- {}".format(np.mean(train_energy_loss)))
+        
+        end_time_epoch = time.time()
+        time_elapsed_epoch = end_time_epoch - start_time_epoch
+
+        print('Time Elapsed for This Epoch: %02d:%02d:%02d' % (time_elapsed_epoch // 3600, \
+                (time_elapsed_epoch % 3600 // 60), (time_elapsed_epoch % 60 // 1)))
+
+        logging.info('Time Elapsed for This Epoch: %02d:%02d:%02d' % (time_elapsed_epoch // 3600, \
+                (time_elapsed_epoch % 3600 // 60), (time_elapsed_epoch % 60 // 1)))
+
+        if epoch % 50 == 0:
+            
+            cur_model_name = model_name+"_"+str(epoch)+".ckpt"
+            model.save(directory=model_dir, filename=cur_model_name)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description = 'Train VariationalCycleGAN model for datasets.')
+
+    emo_dict = {"neu-ang":['neutral', 'angry'], \
+                        "neu-sad":['neutral', 'sad'], \
+                        "neu-hap":['neutral', 'happy']}
+
+    emo_pair = "cmu-arctic"
+    train_dir_default = "./data/"+emo_pair
+    model_dir_default = "./model/"+emo_pair
+    model_name_default = emo_pair
+    tensorboard_log_dir_default = './log/'+emo_pair
+    random_seed_default = 0
+
+    parser.add_argument('--train_dir', type=str, help='Directory for A.', 
+            default=train_dir_default)
+    parser.add_argument('--model_dir', type=str, help='Directory for saving models.', 
+            default=model_dir_default)
+    parser.add_argument('--model_name', type=str, help='File name for saving model.', 
+            default=model_name_default)
+    parser.add_argument('--random_seed', type=int, help='Random seed for model training.', 
+            default=random_seed_default)
+    parser.add_argument('--output_dir', type=str, help='Output directory for converted validation voices.', 
+            default=output_dir_default)
+    parser.add_argument('--tensorboard_log_dir', type=str, help='TensorBoard log directory.', 
+            default=tensorboard_log_dir_default)
+    parser.add_argument('--current_iter', type=int, help="Current iteration of the model (Fine tuning)", 
+            default=1)
+    parser.add_argument('--lambda_energy', type=float, help="hyperparam for loss energy", 
+            default=10.0)
+    parser.add_argument('--generator_learning_rate', type=float, help="generator learning rate", 
+            default=1e-06)
+    parser.add_argument('--discriminator_learning_rate', type=float, help="discriminator learning rate", 
+            default=1e-07)
+    
+    argv = parser.parse_args()
+
+    train_dir = argv.train_dir
+    model_dir = argv.model_dir
+    model_name = argv.model_name
+    random_seed = argv.random_seed
+    output_dir = argv.output_dir
+    tensorboard_log_dir = argv.tensorboard_log_dir
+
+    lambda_energy = argv.lambda_energy
+
+    generator_learning_rate = argv.generator_learning_rate
+    discriminator_learning_rate = argv.discriminator_learning_rate
+
+    train(train_dir=train_dir, model_dir=model_dir, model_name=model_name, 
+          random_seed=random_seed, validation_dir=validation_dir, 
+          output_dir=output_dir, tensorboard_log_dir=tensorboard_log_dir, 
+          pre_train=None, lambda_energy=lambda_energy, 
+          generator_learning_rate=generator_learning_rate, 
+          discriminator_learning_rate=discriminator_learning_rate)
---
+++ modules/modules_supervised_energy_f0_momenta_discriminate_wasserstein.py
@@ -0,0 +1,212 @@
+import tensorflow as tf 
+from modules.base_modules_default_init import *
+
+
+def sampler_pitch(input_pitch, input_mfc, final_filters=1, reuse=False, \
+                       scope_name='sampler_pitch'):
+
+    # Inputs have shape [batch_size, num_features, time]
+    inputs = tf.concat([input_mfc, input_pitch], axis=1, \
+                            name='sampler_input')
+    
+    # Cnvert it to [batch_size, time, num_features] for 1D convolution
+    inputs_tranposed = tf.transpose(inputs, perm = [0, 2, 1], \
+                            name='sampler_input_transpose')
+
+    with tf.variable_scope(scope_name) as scope:
+        # Discriminator would be reused in CycleGAN
+        if reuse:
+            scope.reuse_variables()
+        else:
+            assert scope.reuse is False
+
+        h1 = conv1d_layer(inputs=inputs_tranposed, filters=64, \
+                kernel_size=15, strides=1, \
+                activation=None, name='h1_conv')
+        h1_gates = conv1d_layer(inputs=inputs_tranposed, filters=64, \
+                kernel_size=15, strides=1, \
+                activation=None, name='h1_conv_gates')
+        h1_glu = gated_linear_layer(inputs=h1, \
+                gates=h1_gates, name='h1_glu')
+        
+        # Downsample
+        d1 = downsample1d_block(inputs=h1_glu, filters=128, \
+                kernel_size=5, strides=2, \
+                name_prefix='downsample1d_block1_')
+        d2 = downsample1d_block(inputs=d1, filters=256, \
+                kernel_size=5, strides=2, \
+                name_prefix='downsample1d_block2_')
+
+        # Residual blocks
+        r1 = residual1d_block(inputs=d2, filters=512, \
+                kernel_size=3, strides=1, \
+                name_prefix='residual1d_block1_')
+        r2 = residual1d_block(inputs=r1, filters=512, \
+                kernel_size=3, strides=1, \
+                name_prefix='residual1d_block2_')
+#        r3 = residual1d_block(inputs=r2, filters=512, \
+#                kernel_size=3, strides=1, \
+#                name_prefix='residual1d_block3_')
+
+        # Upsample
+        u1 = upsample1d_block(inputs=r2, filters=512, \
+                kernel_size=5, strides=1, \
+                shuffle_size=2, name_prefix='upsample1d_block1_')
+        u2 = upsample1d_block(inputs=u1, filters=256, \
+                kernel_size=5, strides=1, \
+                shuffle_size=2, name_prefix='upsample1d_block2_')
+        
+        # Dropout for stochasticity
+        u2 = tf.nn.dropout(u2, keep_prob=1.0)
+
+        # Output
+        o1 = conv1d_layer(inputs=u2, filters=final_filters, \
+                kernel_size=15, strides=1, \
+                activation=None, name='o1_conv')
+
+        o2 = tf.transpose(o1, perm=[0, 2, 1], name='output_transpose')
+        return o2
+
+
+def sampler_energy(input_pitch, input_mfc, final_filters=1, reuse=False, \
+                       scope_name='sampler_energy'):
+
+    # Inputs have shape [batch_size, num_features, time]
+    inputs = tf.concat([input_mfc, input_pitch], axis=1, \
+                            name='generator_input')
+    
+    # Cnvert it to [batch_size, time, num_features] for 1D convolution
+    inputs_tranposed = tf.transpose(inputs, perm = [0, 2, 1], \
+                            name='generator_input_transpose')
+
+    with tf.variable_scope(scope_name) as scope:
+        # Discriminator would be reused in CycleGAN
+        if reuse:
+            scope.reuse_variables()
+        else:
+            assert scope.reuse is False
+
+        h1 = conv1d_layer(inputs=inputs_tranposed, filters=64, \
+                kernel_size=15, strides=1, \
+                activation=None, name='h1_conv')
+        h1_gates = conv1d_layer(inputs=inputs_tranposed, filters=64, \
+                kernel_size=15, strides=1, \
+                activation=None, name='h1_conv_gates')
+        h1_glu = gated_linear_layer(inputs=h1, \
+                gates=h1_gates, name='h1_glu')
+        
+        # Downsample
+        d1 = downsample1d_block(inputs=h1_glu, filters=128, \
+                kernel_size=5, strides=2, \
+                name_prefix='downsample1d_block1_')
+        d2 = downsample1d_block(inputs=d1, filters=256, \
+                kernel_size=5, strides=2, \
+                name_prefix='downsample1d_block2_')
+
+        # Residual blocks
+        r1 = residual1d_block(inputs=d2, filters=512, \
+                kernel_size=3, strides=1, \
+                name_prefix='residual1d_block1_')
+        r2 = residual1d_block(inputs=r1, filters=512, \
+                kernel_size=3, strides=1, \
+                name_prefix='residual1d_block2_')
+#        r3 = residual1d_block(inputs=r2, filters=512, \
+#                kernel_size=3, strides=1, \
+#                name_prefix='residual1d_block3_')
+
+        # Upsample
+        u1 = upsample1d_block(inputs=r2, filters=512, \
+                kernel_size=5, strides=1, \
+                shuffle_size=2, name_prefix='upsample1d_block1_')
+        u2 = upsample1d_block(inputs=u1, filters=256, \
+                kernel_size=5, strides=1, \
+                shuffle_size=2, name_prefix='upsample1d_block2_')
+        
+        # Dropout for stochasticity
+        u2 = tf.nn.dropout(u2, keep_prob=1.0)
+
+        # Output
+        o1 = conv1d_layer(inputs=u2, filters=final_filters, \
+                kernel_size=15, strides=1, \
+                activation=None, name='o1_conv')
+
+        o2 = tf.transpose(o1, perm=[0, 2, 1], name='output_transpose')
+        return o2
+    
+
+def discriminator_pitch(input_pitch, reuse=False, scope_name='discriminator_pitch'):
+
+    # input_pitch has shape [batch_size, num_features, time]
+    input_pitch = tf.transpose(input_pitch, perm=[0,2,1], 
+            name='discriminator_pitch_transpose')
+
+    with tf.variable_scope(scope_name) as scope:
+        # Discriminator would be reused in CycleGAN
+        if reuse:
+            scope.reuse_variables()
+        else:
+            assert scope.reuse is False
+
+        h1_pitch = conv1d_layer(inputs=input_pitch, filters=64, 
+                kernel_size=3, strides=1, 
+                activation=None, name='h1_pitch_conv')
+        h1_pitch_gates = conv1d_layer(inputs=input_pitch, filters=64, 
+                kernel_size=3, strides=1, 
+                activation=None, name='h1_pitch_conv_gates')
+        h1_pitch_glu = gated_linear_layer(inputs=h1_pitch, 
+                gates=h1_pitch_gates, name='h1_pitch_glu')
+        
+        d1 = downsample1d_block(inputs=h1_pitch_glu, filters=128, 
+                kernel_size=3, strides=2, 
+                name_prefix='downsample2d_block1_')
+        d2 = downsample1d_block(inputs=d1, filters=256, 
+                kernel_size=3, strides=2, 
+                name_prefix='downsample2d_block2_')
+        d3 = downsample1d_block(inputs=d2, filters=256, 
+                kernel_size=3, strides=2, 
+                name_prefix='downsample2d_block3_')
+
+        # Output
+        o1 = tf.layers.dense(inputs=d3, units=1, \
+                             activation=None)
+
+        return o1
+
+
+def discriminator_energy(input_energy, reuse=False, scope_name='discriminator_energy'):
+
+    # input_energy has shape [batch_size, num_features, time]
+    input_energy = tf.transpose(input_energy, perm=[0,2,1], 
+            name='discriminator_energy_transpose')
+
+    with tf.variable_scope(scope_name) as scope:
+        # Discriminator would be reused in CycleGAN
+        if reuse:
+            scope.reuse_variables()
+        else:
+            assert scope.reuse is False
+
+        h1_energy = conv1d_layer(inputs=input_energy, filters=64, 
+                kernel_size=3, strides=1, 
+                activation=None, name='h1_energy_conv')
+        h1_energy_gates = conv1d_layer(inputs=input_energy, filters=64, 
+                kernel_size=3, strides=1, 
+                activation=None, name='h1_energy_conv_gates')
+        h1_energy_glu = gated_linear_layer(inputs=h1_energy, 
+                gates=h1_energy_gates, name='h1_energy_glu')
+
+        d1 = downsample1d_block(inputs=h1_energy_glu, filters=128, 
+                kernel_size=3, strides=2, 
+                name_prefix='downsample2d_block1_')
+        d2 = downsample1d_block(inputs=d1, filters=256, 
+                kernel_size=3, strides=2, 
+                name_prefix='downsample2d_block2_')
+        d3 = downsample1d_block(inputs=d2, filters=256, 
+                kernel_size=3, strides=2, 
+                name_prefix='downsample2d_block3_')
+
+        # Output
+        o1 = tf.layers.dense(inputs=d3, units=1, \
+                             activation=None)
+
+        return o1
