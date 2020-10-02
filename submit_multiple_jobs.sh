1f3c77149ae545598d61a1d0dd8989739c8e07a4
ravi-0841 <ravishankar0841@gmail.com>
2020-10-02 15:44:15 -0400

    submitting multiple jobs with pre-training

--- submit_multiple_jobs.sh
+++ submit_multiple_jobs.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-cycle_array_pitch=( 1e-06 0.00001 0.0001 )
-cycle_array_energy=( 0.00001 0.001 0.1 )
+cycle_array_pitch=( 1e-06 0.00001 0.0001 0.01 )
+cycle_array_energy=( 1e-06 0.00001 0.001 0.1 )
 
-counter=10
+counter=1
 for p in "${cycle_array_pitch[@]}"
 do
     for e in "${cycle_array_energy[@]}"
