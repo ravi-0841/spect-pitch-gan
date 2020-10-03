49bf295bacd42ede5cc8e38e7b3387fe2e613f35
ravi-0841 <ravishankar0841@gmail.com>
2020-10-03 13:31:55 -0400

    running with higher learning rate for generator

--- gen_disc_job_energy_f0_momenta_discriminate_wasserstein.sh
+++ gen_disc_job_energy_f0_momenta_discriminate_wasserstein.sh
@@ -17,4 +17,4 @@ singularity pull --name tf_1_12.simg shub://ravi-0841/singularity-tensorflow-1.1
 # export singularity home path
 export SINGULARITY_HOME=$PWD:/home/$USER
 
-singularity exec --nv ./tf_1_12.simg python3 main_energy_f0_momenta_discriminate_wasserstein.py --lambda_cycle_pitch $1 --lambda_cycle_energy $2 --lambda_identity_energy 0.0 --lambda_momenta 1e-06 --generator_learning_rate 1e-05 --discriminator_learning_rate 1e-07
+singularity exec --nv ./tf_1_12.simg python3 main_energy_f0_momenta_discriminate_wasserstein.py --lambda_cycle_pitch $1 --lambda_cycle_energy $2 --lambda_identity_energy 0.0 --lambda_momenta 1e-06 --generator_learning_rate 1e-04 --discriminator_learning_rate 1e-06
