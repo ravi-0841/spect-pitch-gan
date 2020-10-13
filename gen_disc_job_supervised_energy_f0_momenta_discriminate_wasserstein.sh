9f905a9465dfedee8c410f16b4bace8c1ca7b19a
ravi-0841 <ravishankar0841@gmail.com>
2020-10-12 20:25:29 -0400

    dimensionality issues in pitch and ec

--- gen_disc_job_supervised_energy_f0_momenta_discriminate_wasserstein.sh
+++ gen_disc_job_supervised_energy_f0_momenta_discriminate_wasserstein.sh
@@ -17,4 +17,4 @@ singularity pull --name tf_1_12.simg shub://ravi-0841/singularity-tensorflow-1.1
 # export singularity home path
 export SINGULARITY_HOME=$PWD:/home/$USER
 
-singularity exec --nv ./tf_1_12.simg python3 main_supervised_energy_f0_momenta_discriminate_wasserstein.py --lambda_energy $1 --generator_learning_rate $2 --discriminator_learning_rate 1e-06
+singularity exec --nv ./tf_1_12.simg python3 main_supervised_energy_f0_momenta_discriminate_wasserstein.py --lambda_energy $1 --lambda_momenta_pitch $2 --lambda_momenta_energy $3 --generator_learning_rate $4 --discriminator_learning_rate 1e-06
