286cf527dd1ea99f6334197280718594897aab4c
ravi-0841 <ravishankar0841@gmail.com>
2020-10-17 16:45:54 -0400

    Removed mean subtraction

--- main_energy_f0_momenta_discriminate_wasserstein.py
+++ main_energy_f0_momenta_discriminate_wasserstein.py
@@ -41,7 +41,7 @@ def train(train_dir, model_dir, model_name, random_seed, \
             + '_li_'+str(lambda_identity_energy) \
             +'_lrg_'+str(generator_learning_rate) \
             +'_lrd_'+str(discriminator_learning_rate) \
-            + '_spect_ec_mean_sub'
+            + '_spect_ec'
 
     model_dir = os.path.join(model_dir, lc_lm)
 
@@ -91,10 +91,10 @@ def train(train_dir, model_dir, model_name, random_seed, \
     mfc_A_valid = data_valid['src_mfc_feat']
     mfc_B_valid = data_valid['tar_mfc_feat']
 
-    pitch_A_train = pitch_A_train - np.mean(pitch_A_train, axis=-1, keepdims=True) 
-    pitch_B_train = pitch_B_train - np.mean(pitch_B_train, axis=-1, keepdims=True) 
-    pitch_A_valid = pitch_A_valid - np.mean(pitch_A_valid, axis=-1, keepdims=True) 
-    pitch_B_valid = pitch_B_valid - np.mean(pitch_B_valid, axis=-1, keepdims=True) 
+#    pitch_A_train = pitch_A_train - np.mean(pitch_A_train, axis=-1, keepdims=True) 
+#    pitch_B_train = pitch_B_train - np.mean(pitch_B_train, axis=-1, keepdims=True) 
+#    pitch_A_valid = pitch_A_valid - np.mean(pitch_A_valid, axis=-1, keepdims=True) 
+#    pitch_B_valid = pitch_B_valid - np.mean(pitch_B_valid, axis=-1, keepdims=True) 
 
     # Randomly shuffle the trainig data
     indices_train = np.arange(0, pitch_A_train.shape[0])
