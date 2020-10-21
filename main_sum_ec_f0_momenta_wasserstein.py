c0116ea17570959899d16acd615474644eed0519
ravi-0841 <ravishankar0841@gmail.com>
2020-10-21 17:51:17 -0400

    running wasserstein with gender shuffling

--- main_sum_ec_f0_momenta_wasserstein.py
+++ main_sum_ec_f0_momenta_wasserstein.py
@@ -115,7 +115,7 @@ def train(train_dir, model_dir, model_name, random_seed, \
 #    mfc_B_train = mfc_B_train[indices_train]
     mfc_A_train, mfc_B_train, pitch_A_train, pitch_B_train, \
             energy_A_train, energy_B_train = preproc.gender_shuffle(mfc_A=mfc_A_train, 
-                    mfc_B=mfc_B_train, pitch_A=pitch_A_trian, pitch_B=pitch_B_train, 
+                    mfc_B=mfc_B_train, pitch_A=pitch_A_train, pitch_B=pitch_B_train, 
                     energy_A=energy_A_train, energy_B=energy_B_train, files=files, cutoff=1260)
 
     mfc_A_valid, pitch_A_valid, energy_A_valid, \
