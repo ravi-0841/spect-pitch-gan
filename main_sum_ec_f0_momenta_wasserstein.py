16d95449fe7f1ecbbe88d083f1ca7b346a851f64
ravi-0841 <ravishankar0841@gmail.com>
2020-11-18 10:14:47 -0500

    run 21 seed again

--- main_sum_ec_f0_momenta_wasserstein.py
+++ main_sum_ec_f0_momenta_wasserstein.py
@@ -42,7 +42,7 @@ def train(train_dir, model_dir, model_name, random_seed, \
             + '_li_'+str(lambda_identity_energy) \
             +'_lrg_'+str(generator_learning_rate) \
             +'_lrd_'+str(discriminator_learning_rate) \
-            + '_sum_mfc_'+emo_pair+'_random_seed_'+str(tf_random_seed)
+            + '_sum_mfc_'+emo_pair+'_random_seed_2_'+str(tf_random_seed)
 
     folder_extension = 'sum_mfc_wstn_'+emo_pair+'_random_seed/'
 
