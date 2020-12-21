d342f342e924d5daa23b5bd6ce9487b69b7d4326
ravi-0841 <ravishankar0841@gmail.com>
2020-12-21 12:09:31 -0500

    loaded models for model averaging: discrepancy persists

--- utils/model_averaging.py
+++ utils/model_averaging.py
@@ -5,13 +5,13 @@ session_avg = tf.Session()
 
 
 with session_gend as sess:
-    loader1 = tf.train.import_meta_graph('/home/ravi/Desktop/sum_mfc_models/neu-ang/lp_0.0001_le_1e-06_li_0.0_lrg_1e-05_lrd_1e-07_sum_mfc_gender_shuffled/neu-ang_200.ckpt.meta')
-    loader1.restore(sess, '/home/ravi/Desktop/sum_mfc_models/neu-ang/lp_0.0001_le_1e-06_li_0.0_lrg_1e-05_lrd_1e-07_sum_mfc_gender_shuffled/neu-ang_200.ckpt')
+    loader1 = tf.train.import_meta_graph('/home/ravi/Desktop/mixing_models/lp_1e-05_le_0.1_li_0.0_lrg_1e-05_lrd_1e-07_sum_mfc_gender_neu-ang_random_seed_5/neu-ang_200.ckpt.meta')
+    loader1.restore(sess, '/home/ravi/Desktop/mixing_models/lp_1e-05_le_0.1_li_0.0_lrg_1e-05_lrd_1e-07_sum_mfc_gender_neu-ang_random_seed_5/neu-ang_200.ckpt')
     var_gend = sess.run(tf.trainable_variables())
 
 with session_unif as sess:
-    loader2 = tf.train.import_meta_graph('/home/ravi/Desktop/sum_mfc_models/neu-ang/lp_1e-05_le_0.1_li_0.0_lrg_1e-05_lrd_1e-07_sum_mfc_best/neu-ang_200.ckpt.meta')
-    loader2.restore(sess, '/home/ravi/Desktop/sum_mfc_models/neu-ang/lp_1e-05_le_0.1_li_0.0_lrg_1e-05_lrd_1e-07_sum_mfc_best/neu-ang_200.ckpt')
+    loader2 = tf.train.import_meta_graph('/home/ravi/Desktop/mixing_models/lp_1e-05_le_0.1_li_0.0_lrg_1e-05_lrd_1e-07_sum_mfc_neu-ang_random_seed_3_2_21/neu-ang_200.ckpt.meta')
+    loader2.restore(sess, '/home/ravi/Desktop/mixing_models/lp_1e-05_le_0.1_li_0.0_lrg_1e-05_lrd_1e-07_sum_mfc_neu-ang_random_seed_3_2_21/neu-ang_200.ckpt')
     var_unif = sess.run(tf.trainable_variables())
 
 
