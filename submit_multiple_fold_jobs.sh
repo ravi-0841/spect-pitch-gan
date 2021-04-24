fb2022d2a80646e21a902aec8e00fbb8ba299843
ravi-0841 <ravishankar0841@gmail.com>
2021-04-24 17:52:18 -0400

    neutral-happy job submitted

--- submit_multiple_fold_jobs.sh
+++ submit_multiple_fold_jobs.sh
@@ -7,7 +7,7 @@ do
     for c in {1..4..1}
     do
         echo $counter;
-        sbatch -J $counter -o "./txt_files/NA_fold_${f}_count_${c}.txt" gen_disc_job_sum_ec_f0_fold.sh 1e-05 0.1 21 neu-ang $f $c
+        sbatch -J $counter -o "./txt_files/NH_fold_${f}_count_${c}.txt" gen_disc_job_sum_ec_f0_fold.sh 0.0001 0.001 4 neu-hap $f $c
         counter=$((counter+1))
     done
 done
