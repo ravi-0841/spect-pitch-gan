ba96daf5f45e5d0f5d7fb6c95458eef49c9a76ff
ravi-0841 <ravishankar0841@gmail.com>
2021-05-23 14:02:14 -0400

    model evaluation and submit jobs edited

--- submit_evaluation_jobs.sh
+++ submit_evaluation_jobs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#for f in {1..5..1}
-#do
-#	for r in {1..4..1}
-#	do
-#		sbatch -J NA_$f_$r -o "./txt_files/NA_fold_${f}_run_${r}.txt" evaluate.sh neu-ang $f $r
-#	done
-#done
+for f in {1..5..1}
+do
+	for r in {1..4..1}
+	do
+		sbatch -J NA_$f_$r -o "./txt_files/NA_fold_${f}_run_${r}.txt" evaluate.sh neu-ang $f $r
+	done
+done
 
 for f in {1..5..1}
 do
