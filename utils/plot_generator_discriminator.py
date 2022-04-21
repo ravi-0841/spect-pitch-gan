b12d4014fb3c9eb209c8697b600fadf5082641d6
ravi-0841 <ravishankar0841@gmail.com>
2022-04-21 10:30:33 -0400

    plotting updates

--- utils/plot_generator_discriminator.py
+++ utils/plot_generator_discriminator.py
@@ -30,6 +30,8 @@ if __name__=="__main__":
         train_mfc_A2B_loss = list()
         train_mfc_B2A_loss = list()
 
+        diff = list()
+
         r = open(f,'r')
         for line in r:
             try:
@@ -57,16 +59,22 @@ if __name__=="__main__":
                 if "Train Mfc B2A" in line:
                     n = re.findall(r"[-+]?\d*\.\d+e-\d+|[-+]?\d*\.\d+|\d+", line)
                     train_mfc_B2A_loss.append(float(n[1]))
+
+                diff.append(np.abs(train_generator_loss[-1] - train_discriminator_loss[-1]))
             except Exception as e:
                 pass
 
         r.close()
         if len(train_generator_loss)>0:
             pylab.figure(figsize=(12,12))
+            pylab.subplot(121)
             pylab.plot(train_generator_loss, 'r', label="generator loss")
             pylab.plot(train_discriminator_loss, 'g', label="discriminator loss")
             pylab.grid(), pylab.legend(loc=1)
             pylab.title(name)
+            pylab.subplot(122)
+            pylab.plot(diff, 'g', label='Difference loss')
+            pylab.title('Difference')
             pylab.savefig(args.dir+name+'.png')
             pylab.close()
         elif len(train_momenta_A2B_loss)>0:
